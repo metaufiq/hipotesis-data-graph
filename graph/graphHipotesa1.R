028796aa49ee83f127561d637851621abca225e1
taufiq <taufiq1689@gmail.com>
2020-05-01 21:47:11 +0800

    hipotesa 1 selesai

--- graph/graphHipotesa1.R
+++ graph/graphHipotesa1.R
@@ -1,12 +1,43 @@
 library(igraph)
 
-setwd("D:\\Project\\hipotesis-data-graph\\graph")
+setwd("D:\\Project\\hipotesis-data-graph")
 
 # relation = as.matrix(read.csv("relasi.csv",header=FALSE, sep=",",stringsAsFactors=FALSE))
-nodes <- read.csv("nodes.csv",header=TRUE, sep=",",as.is=TRUE)
-links <- read.csv("relasiGraph.csv", header=FALSE, sep=",",as.is=TRUE)
+data = as.matrix(read.csv("atribut.csv", sep=","))
 
-net <- graph_from_data_frame(d=links, vertices=nodes, directed=FALSE)
-# deg <- degree(net, mode="all")
-print(V(net))
-plot(net, vertex.size=deg*3)
+productId = data[,1]
+reviewUserId = data[,2]
+
+
+relasi = as.matrix(read.csv("relasi.csv",header=FALSE, sep=","))
+#buat graph
+gpokec <- graph_from_adjacency_matrix(relasi)
+
+#hipotesa 1
+gpokec.productId <- productId
+gpokec.reviewUserId <- reviewUserId
+
+
+gpokec <- set.vertex.attribute(gpokec,'productId', index = V(gpokec), 
+                               gpokec.productId)
+gpokec <- set.vertex.attribute(gpokec,'reviewUserId', index = V(gpokec),
+                               gpokec.reviewUserId)
+
+V(gpokec)$reviewUserId[grepl("A3UCN2RGY7O6S1",V(gpokec)$reviewUserId)] <- 1
+V(gpokec)$reviewUserId[V(gpokec)$reviewUserId != 1] <- 2
+
+
+
+colrs <- c("tomato", "gold")
+V(gpokec)$color <- colrs[as.integer(V(gpokec)$reviewUserId)]
+deg <- degree(gpokec, mode="all")
+print(deg)
+V(gpokec)$size <- deg
+V(gpokec)$label <- NA
+
+E(gpokec)$arrow.size <- .2
+E(gpokec)$edge.color <- "gray80"
+l <- layout_with_graphopt(gpokec)
+plot(gpokec,layout=l)
+legend(x=-1.5, y=-1.1, c("Direview","Tidak direview"), pch=21,
+       col="#777777", pt.bg=colrs, pt.cex=2, cex=.8, bty="n", ncol=1)
